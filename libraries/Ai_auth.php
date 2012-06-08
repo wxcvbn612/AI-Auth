e7642b60a7530f5dbcf49bf3045223320001339f
Jait Aimad <jait.aimad@gmail.com>
2012-06-08 02:13:04 +0000

    add 1st file

---
+++ libraries/Ai_auth.php
@@ -0,0 +1,19 @@
+<?php
+ob_start();
+/**
+* The EasyClick Authentication Library
+*
+* @package Authentication
+* @category Libraries
+* @author Easyclick.ma
+* @link http://easyclick.ma/ci
+* @version 0.0.1
+* @copyright Easyclick.ma 2012
+*
+* Auth provides a powerful, lightweight and simple interface for user authentication 
+*/
+
+class Ai_auth
+{
+    
+}
